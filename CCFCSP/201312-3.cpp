41fa1eff73377442f424e21818399d349b2fe731
Astral <astralhope@gmail.com>
2020-08-17 17:07:15 +0800

    201312-3 最大的矩形 90分

---
+++ CCFCSP/201312-3.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h> 
+using namespace std;
+int area(vector<int>& v,int pos){
+	int counter=1;
+	for(int i=pos-1;i>0;i--){
+		if(v[i]>=v[pos]){counter++;}else{break;}
+	}
+	for(int i=pos+1;i<v.size();i++){
+		if(v[i]>=v[pos]){counter++;}else{break;}
+	}
+	return counter*v[pos];
+}
+
+int main(){
+	int n,tmp;
+	vector<int> v;
+	cin>>n;
+	while(n--){
+		cin>>tmp;
+		v.push_back(tmp);
+	}
+	int ans=0;
+	for(int i=0;i<v.size();i++){
+		if(ans<area(v,i)){
+			ans=area(v,i);
+		}
+	}
+	cout<<ans;
+	return 0;
+}
