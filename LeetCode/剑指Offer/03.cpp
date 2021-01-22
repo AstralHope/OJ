6b86bcade54c9f3b0ae252f4074c7e5f9505f08c
Astral <astralhope@gmail.com>
2021-01-22 20:22:39 +0800

    剑指 Offer 03 数组中重复的数字
    增加注释内容

--- LeetCode/剑指Offer/03.cpp
+++ LeetCode/剑指Offer/03.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    int findRepeatNumber(vector<int>& nums) {
-        int n = nums.size();
+    int findRepeatNumber(vector<int>& nums) {//题目给的数字都在数组nums里
+        int n = nums.size();//获取数组的长度n
         int *a;
-        a = new int[n];
-        while(n>0){
+        a = new int[n];//创建一个长度为n的数组a
+        while(n>0){//将a的每一项初始化为0，例如示例中给的例子就创建了一个数组a=[0,0,0,0,0,0,0]
             a[--n]=0;
         }
+        
         //直接用a[x]的值表示x出现的次数，当x>1时出现重复，得到结果
+        
         for(int i=0;i<nums.size();i++){
+            //从0开始，依此访问nums[i]，
             if(a[nums[i]]==0){
-                a[nums[i]]++;
-            }else{
+                a[nums[i]]++;//例如示例中nums[0]=2，就把a[2]的值+1；第二次循环来的时候a[3]的值+1；第三次和第四次的分别a[1]+1;a[0]+1,这个时候a=[1,1,1,1,0,0,0]
+            }else{//第五次访问的时候num[4]=2,这个时候a[2]已经等于1了，不满足if的条件，就执行else的内容，返回num[4]的值2，函数运行结束
                 return nums[i];
             }
         }
